246cd13d63d7860529822c5cf245f76e4873fd1f
Carrie Kraft <carriekarft@gmail.com>
2023-07-24 20:14:12 -0700

    commit tree 7a09f5a0cd17106f41bb3abec056c480002dd0c7

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,13 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    # validates :name, uniqueness: true
+    validates :phone_number, length: {is: 10}
+
 end
+#     All authors have a name
+# No two authors have the same name
+# Author phone numbers are exactly ten digits
+# All posts have a title
+# Post content is at least 250 characters long
+# Post summary is a maximum of 250 characters
+# Post category is either Fiction or Non-Fiction. This step requires an inclusion validator, which was not outlined in the lesson. You'll need to refer to the Rails guideLinks to an external site. to look up how to use it.
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,34 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+    end
 end
+#     All authors have a name
+# No two authors have the same name
+# Author phone numbers are exactly ten digits
+# All posts have a title
+# Post content is at least 250 characters long
+# Post summary is a maximum of 250 characters
+# Post category is either Fiction or Non-Fiction. This step requires an inclusion validator, which was not outlined in the lesson. You'll need to refer to the Rails guideLinks to an external site. to look up how to use it.
+
+# Finally, add a custom validator to the Post model that ensures the title is sufficiently clickbait-y. The validator should add a validation error if the title does not contain:
+
+# "Won't Believe"
+# "Secret"
+# "Top [number]"
+# "Guess"
